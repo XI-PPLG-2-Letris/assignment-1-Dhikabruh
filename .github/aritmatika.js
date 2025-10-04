2499a800e5648014b21dd2784bd0b51c34c72c07
Dhikabruh <muhammadhikaaa@gmail.com>
2025-10-04 19:03:07 +0700

    yapp

---
+++ .github/aritmatika.js
@@ -0,0 +1,37 @@
+a = 10
+b = 20
+
+const penjumlahan = () => {
+    console.log(`hasil dari ${a} + ${b} adalah = ${a + b}`);
+}
+penjumlahan()
+
+
+const pengurangan = () => {
+    console.log(`hasil dari ${a} - ${b} adalah = ${a - b}`);
+}
+pengurangan()
+
+
+const perkalian = () => {
+    console.log(`hasil dari ${a} x ${b} adalah = ${a * b}`);
+}
+perkalian()
+
+
+const modulus = () => {
+    console.log(`hasil dari ${a} % ${b} adalah = ${a % b}`);
+}
+modulus()
+
+
+const pembagian = () => {
+    console.log(`hasil dari ${a} : ${b} adalah = ${a / b}`);
+}
+pembagian()
+
+
+const pangkat = () => {
+    console.log(`hasil dari ${a} ** ${b} adalah = ${a ** b}`);
+}
+pangkat()
